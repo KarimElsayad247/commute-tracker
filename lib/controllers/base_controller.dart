a5cbb016e5becbb8fa15850fe2643f3597ebfc34
KarimElsayad247 <karimelsayad1999@gmail.com>
2023-01-26 20:09:54 +0200

    Refactor controllers to move all() to base class
    This refactor would allow implementing more methods that would
    be shared across all controllers.

--- lib/controllers/base_controller.dart
+++ lib/controllers/base_controller.dart
@@ -1,7 +1,14 @@
 import 'package:commute_tracker/main.dart';
 import 'package:commute_tracker/models/models.dart';
+import 'package:drift/drift.dart' as d;
 import 'package:flutter/cupertino.dart';
 
-class BaseController extends ChangeNotifier {
+abstract class BaseController<T extends d.Table, D> extends ChangeNotifier {
   Database get db => getIt<Database>();
+
+  d.TableInfo<T, D> get table;
+
+  Future<List<D>> all() async {
+    return db.select(table).get();
+  }
 }
--- lib/controllers/commute_routes_controller.dart
+++ lib/controllers/commute_routes_controller.dart
@@ -1,31 +1,20 @@
 import 'dart:async';
 
 import 'package:commute_tracker/controllers/base_controller.dart';
-import 'package:commute_tracker/main.dart';
 import 'package:drift/drift.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
 
 import '../models/models.dart';
 
-class CommuteRoutesController extends BaseController {
-  static const String tableName = 'routes';
+class CommuteRoutesController
+    extends BaseController<$CommuteRoutesTable, CommuteRoute> {
+  @override
+  TableInfo<$CommuteRoutesTable, CommuteRoute> get table => db.commuteRoutes;
 
   Future<void> create({required title, description}) async {
     CommuteRoutesCompanion route = CommuteRoutesCompanion(
         title: Value(title), description: Value(description));
     db.into(db.commuteRoutes).insert(route);
     notifyListeners();
-  }
-
-  Future<List<CommuteRoute>> all() async {
-    return db.select(db.commuteRoutes).get();
-  }
-
-  Future<CommuteRoute?> get(int id) async {
-    final query = db.select(db.commuteRoutes)
-      ..where((route) => route.id.equals(id));
-    return query.getSingle();
   }
 
   Future<void> updateRoute(int id, String title, String description) async {
@@ -49,10 +38,5 @@ class CommuteRoutesController extends BaseController {
     );
     db.into(db.routeSegments).insert(segment);
     notifyListeners();
-  }
-
-  CommuteRoute fromMap(Map<String, dynamic> map) {
-    return CommuteRoute(
-        id: map['id'], title: map['title'], description: map['description']);
   }
 }
